21b14d9b3df6613e8ab9ad723f524d7fa740ab83
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2024-05-28 20:57:12 -0600

    dual servers

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const goatApp = express();
+const goatPort = 3000;
 
 let goatCount = 0;
 
-app.get('/', (request, response) => {
-  console.log('request.data:', request?.data);
-  console.log('request.url:', request?.url);
+goatApp.get('/', (request, response) => {
+  console.log('goat request.data:', request?.data);
+  console.log('goat request.url:', request?.url);
   goatCount++;
-  response.send(`Hello World! This page has been visited by ${goatCount} users.`);
+  response.send(`Hello World! This page has been visited by ${goatCount} goats.`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+goatApp.listen(goatPort, () => {
+  console.log(`Example app listening on port ${goatPort}`)
+});
+
+
+const catApp = express();
+const catPort = 9000;
+
+let catCount = 0;
+
+catApp.get('/', (request, response) => {
+  console.log('cat request.data:', request?.data);
+  console.log('cat request.url:', request?.url);
+  catCount++;
+  response.send(`Hello World! This page has been visited by ${catCount} cats.`);
+});
+
+catApp.listen(catPort, () => {
+  console.log(`Example app listening on port ${catPort}`)
 });
