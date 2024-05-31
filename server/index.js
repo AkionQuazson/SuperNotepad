6a7e3c60ec2edda3c236fb5b7ca16ab5d217eb98
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2024-05-30 21:20:37 -0600

    Started client side, RESTful interface for notes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const state = require('./persistence');
 
-
 const app = express();
 const port = 3000;
 
+// for serving public directory at /
+app.use(express.static('./public'));
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (request, response) => {
+
+app.get('/notes/', (request, response) => {
 	console.log('incoming request', request.url);
-	state.notes.push(request.url);
-	response.json(state);
+	// state.notes.push(request.url);
+	response.json(state.notes);
+});
+app.post('/notes/', (request, response) => {
+	console.log('request.body', request.body);
+	let note = request.body?.note.trim() || '';
+	if (!note) {
+		response.status(400).json({error:'note must not be empty'});
+		return;
+	}
+	state.notes.push(note);
+	response.json(state.notes);
 });
 
 app.listen(port, () => {
